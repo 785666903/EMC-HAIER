2be72dd1327c7dc17b74ede7a63caca2bf7ccacd
孙晓昱 <785666903@qq.com>
2017-07-26 23:09:13 +0800

    EMC

---
+++ EMC-HAIER/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  EMC-HAIER
+//
+//  Created by sun_sxy_7 on 2017/7/26.
+//  Copyright © 2017年 sun_sxy_7. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ EMC-HAIER/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EMC-HAIER
+//
+//  Created by sun_sxy_7 on 2017/7/26.
+//  Copyright © 2017年 sun_sxy_7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
